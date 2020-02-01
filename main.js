ae5a87d59f6f89d145eeb8aa5ec83c41c3889ff9
Alex Eickelman <aeickelman40@gmail.com>
2020-02-01 13:22:35 -0700

    Add step 4 tasks

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ closeButton.addEventListener('click', closeWelcome);
 function closeWelcome() {
   welcomeDisappear.style.display = "none";
 }
+
+
+var wallet = document.querySelector('.transactions');
+var clipboard = document.querySelector('.dashboard');
+var main = document.querySelector('.main');
+
+wallet.addEventListener('click', borderToWallet);
+
+
+function borderToWallet () {
+  clipboard.classList.remove('navImageBorder');
+  wallet.classList.add('navImageBorder');
+  main.classList.add('hide');
+}
+
+clipboard.addEventListener('click', walletToClipboard);
+
+function walletToClipboard () {
+  clipboard.classList.add('navImageBorder');
+  wallet.classList.remove('navImageBorder');
+  main.classList.remove('hide'); 
+}
